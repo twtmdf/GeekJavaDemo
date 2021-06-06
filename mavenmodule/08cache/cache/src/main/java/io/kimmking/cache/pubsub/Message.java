7da1919ceb02de377167ca0fbfc5e1be66813315
xuy <xuy@startimes.com.cn>
2021-06-06 21:06:55 +0800

    9.（必做）基于 Redis 的 PubSub 实现订单异步处理,未测试待补充

--- mavenmodule/08cache/cache/src/main/java/io/kimmking/cache/pubsub/Message.java
+++ mavenmodule/08cache/cache/src/main/java/io/kimmking/cache/pubsub/Message.java
@@ -6,9 +6,11 @@ import org.springframework.stereotype.Service;
 public class Message {
     private MyRedisPubSubListener redisPubSubListener;
     public void putMessage() {
+//        发下单消息
         redisPubSubListener.message("key","message");
     }
     public void subMessage() {
+//        消费消息处理订单
         redisPubSubListener.subscribed("key",1);
     }
 }
